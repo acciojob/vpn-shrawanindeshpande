eaeffdd924232a5cdff5b80d12f76438ac31d0dc
shrawani deshpande <shrawanid2000@gmail.com>
2024-03-03 22:55:40 +0530

    first commit

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -1,1 +1,83 @@
+package com.driver.model;// Note: Do not write @Enumerated annotation above CountryName in this model.
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private CountryName countryName;
+
+    private String code;
+
+    @ManyToOne
+    @JoinColumn
+    private ServiceProvider serviceProvider;
+
+
+    @OneToOne
+    @JoinColumn
+    private User user;
+
+    public Country() {
+    }
+
+    public Country(CountryName countryName, String code) {
+        this.countryName = countryName;
+        this.code = code;
+    }
+
+    public Country(int id, CountryName countryName, String code, ServiceProvider serviceProvider, User user) {
+        this.id = id;
+        this.countryName = countryName;
+        this.code = code;
+        this.serviceProvider = serviceProvider;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CountryName getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(CountryName countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+
+    public void setServiceProvider(ServiceProvider serviceProvider) {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
 // Note: Do not write @Enumerated annotation above CountryName in this model.
--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,11 +1,18 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -20,13 +27,55 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+
+        Admin admin = new Admin();
+        admin.setUsername(username);
+        admin.setPassword(password);
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+
+        Optional<Admin> optionalAdmin = adminRepository1.findById(adminId);
+        Admin admin = optionalAdmin.get();
+
+        ServiceProvider serviceProvider = new ServiceProvider();
+
+        serviceProvider.setName(providerName);
+        serviceProvider.setAdmin(admin);
+        List<ServiceProvider> serviceProviderList = admin.getServiceProviders();
+        serviceProviderList.add(serviceProvider);
+
+        admin.setServiceProviders(serviceProviderList);
+
+        adminRepository1.save(admin);
+        return admin;
+
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+
+
+        String str = countryName.toUpperCase();
+        if(!str.equals("IND") && !str.equals("JPN") && !str.equals("AUS") && !str.equals("CHI") && !str.equals("USA")){
+            throw new Exception("Country not found");
+        }
+        Optional<ServiceProvider> optionalServiceProvider = serviceProviderRepository1.findById(serviceProviderId);
+
+        ServiceProvider serviceProvider = optionalServiceProvider.get();
+
+        Country country = new Country(CountryName.valueOf(str),CountryName.valueOf(str).toCode());
+
+
+        country.setServiceProvider(serviceProvider);
+
+        serviceProvider.getCountryList().add(country);
+
+        serviceProviderRepository1.save(serviceProvider);
+        return serviceProvider;
+
     }
 }
